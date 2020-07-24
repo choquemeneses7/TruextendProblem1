d80a591eb8a7bb6b87264d9f35291a33379d670d
Veimar Choque <veimarlala1@gmail.com>
2020-07-24 12:22:41 -0400

    [develop] Create CRUD services for ClassService

---
+++ src/main/java/com/tx/TruextendProblem1/services/ClassService.java
@@ -0,0 +1,43 @@
+package com.tx.TruextendProblem1.services;
+
+import com.tx.TruextendProblem1.entities.Class;
+import com.tx.TruextendProblem1.entities.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClassService {
+    private List<Class> classesList= new ArrayList<Class>(Arrays.asList(
+            new Class(1, "Math", "Simple Math Class"),
+            new Class(2, "Music", "Rock Alternative Class"),
+            new Class(3, "Art", "Painting Mosaics Class"),
+            new Class(4, "Design", "Photoshop and illustrator class"),
+            new Class(5, "Training", "A programation training program")
+    ));
+
+    public List<Class> getClasses() {
+        return classesList;
+    }
+
+    public Class createClass(Class newClass){
+        classesList.add(newClass);
+        return newClass;
+    }
+
+    public void deleteClass(int classCode){
+        classesList.removeIf(itemClass -> itemClass.getCode() == classCode);
+    }
+
+    public Class updateStudent(int classCode, Class newClass) {
+        Optional<Class> studentFind = classesList.stream().filter(itemClass -> classCode == itemClass.getCode()).findFirst();
+        if (studentFind.isPresent()){
+            studentFind.map(itemClass -> classesList.set(classesList.indexOf(itemClass), newClass));
+            return newClass;
+        }
+        return null;
+    }
+}
