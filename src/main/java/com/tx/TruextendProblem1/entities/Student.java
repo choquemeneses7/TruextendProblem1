e9249823ecd2d8c1fac8923cf88900ec886fc749
Veimar Choque <veimarlala1@gmail.com>
2020-07-24 01:26:38 -0400

    [develop] Created student entity

---
+++ src/main/java/com/tx/TruextendProblem1/entities/Student.java
@@ -0,0 +1,41 @@
+package com.tx.TruextendProblem1.entities;
+
+public class Student {
+
+    private int studentId;
+    private String lastName;
+    private String firstName;
+
+    public Student(int studentId, String lastName, String firstName) {
+        this.studentId = studentId;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public Student() {
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
