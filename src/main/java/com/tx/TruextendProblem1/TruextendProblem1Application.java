dacd25fca9025bd86788289d5f16a858e5cda14e
Veimar Choque <veimarlala1@gmail.com>
2020-07-24 01:18:23 -0400

    [master] Add Initial files of Java Spring Project

---
+++ src/main/java/com/tx/TruextendProblem1/TruextendProblem1Application.java
@@ -0,0 +1,13 @@
+package com.tx.TruextendProblem1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruextendProblem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruextendProblem1Application.class, args);
+	}
+
+}
